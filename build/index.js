1ebe6e1d5ddd7d8bf25f986ecfaf62bfc5c24ce3
Alan Smith <a.bax.smith@gmail.com>
2016-12-22 17:02:07 -0700

    makes the api a little more friendly

--- build/index.js
+++ build/index.js
@@ -1,13 +1,11 @@
 'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
 var _SomeComponent = require('./components/SomeComponent');
 
 var _SomeComponent2 = _interopRequireDefault(_SomeComponent);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.default = _SomeComponent2.default;
+module.exports = {
+  SomeComponent: _SomeComponent2.default
+};
